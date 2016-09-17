20ec67c261d7cd6b97df9f224031b11b2adc098f
Rafaell Lycan <sonny.webdsg@gmail.com>
2016-09-17 03:23:21 -0300

    Create default tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,9 @@
 const gulp       = require('gulp');
 const requireDir = require('require-dir');
 const tasks      = requireDir('./gulp/tasks', {recurse: true});
+
+gulp.task('assets', ['styles', 'scripts']);
+
+gulp.task('build', ['clean', 'assets', 'jekyll', 'html']);
+
+gulp.task('default', ['build', 'serve', 'watch']);
