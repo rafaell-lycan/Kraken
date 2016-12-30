b26490972465ba94884c816a81aa35e410d15d9c
Rafaell Lycan <sonny.webdsg@gmail.com>
2016-12-30 19:04:27 +0100

    Change gulp to ES6 with Babel

---
+++ gulpfile.babel.js
@@ -0,0 +1,26 @@
+import config from './taskconfig';
+import gulp from 'gulp';
+import sequence from 'run-sequence';
+import $util from 'gulp-util';
+
+import requireDir from 'require-dir';
+
+requireDir('./gulp/tasks', {recurse: true});
+
+/**
+ * Build task. This is the task that gets executed when you run the shell
+ * command 'gulp build'. This task will wipe the compiled files and rebuild
+ * everything, with on-complete options such as serving the app in the dev
+ * server.
+ *
+ * @param {boolean} debug
+ * @param {boolean} watch
+ */
+gulp.task('default', () => {
+  let seq = ['images', 'svgs', 'videos', 'fonts', 'styles', 'scripts'];
+
+  if (config.env.debug) seq.push('watch');
+  if (!config.env.debug) seq.push('rev');
+  sequence.apply(null, seq);
+});
+
