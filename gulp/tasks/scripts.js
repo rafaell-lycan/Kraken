97b0e92a3ff65158ee3495e3a9555376873b4292
Rafaell Lycan <sonny.webdsg@gmail.com>
2016-12-30 19:05:13 +0100

    Change scripts task with ES6/Babel support

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,45 +1,41 @@
 'use strict';
-const argv         = require('yargs').argv;
-const concat       = require('gulp-concat');
-const gulp         = require('gulp');
-const gzip         = require('gulp-gzip');
-const newer        = require('gulp-newer');
-const rename       = require('gulp-rename');
-const rev          = require('gulp-rev');
-const size         = require('gulp-size');
-const sourcemaps   = require('gulp-sourcemaps');
-const uglify       = require('gulp-uglify');
-const when         = require('gulp-if');
 
-gulp.task('scripts', () =>
-  // NOTE: The order here is important since it's concatenated in order from
-  // top to bottom, so you want vendor scripts etc on top
-  gulp.src([
-    'src/assets/js/vendor/jquery/*.js',
-    'src/assets/js/plugins/**/*.js',
-    'src/assets/js/main.js'
-  ])
-    .pipe(newer('dist/assets/js/index.js', {dest: 'dist/assets/js', ext: '.js'}))
-    .pipe(when(!argv.prod, sourcemaps.init()))
-    .pipe(concat('index.js'))
-    .pipe(size({
-      title: '[scripts] concatenate scripts into index file',
-      showFiles: true
-    }))
-    .pipe(when(argv.prod, rename({suffix: '.min'})))
-    .pipe(when(argv.prod, when('*.js', uglify({preserveComments: 'some'}))))
-    .pipe(when(argv.prod, size({
-      title: '[scripts] minify js',
-      showFiles: true
-    })))
-    .pipe(when(argv.prod, rev()))
-    .pipe(when(!argv.prod, sourcemaps.write('.')))
-    .pipe(when(argv.prod, gulp.dest('dist/assets/js')))
-    .pipe(when(argv.prod, when('*.js', gzip({append: true}))))
-    .pipe(when(argv.prod, size({
-      title: '[scripts] generate gzip files',
-      gzip: true,
-      showFiles: true
-    })))
-    .pipe(gulp.dest('dist/assets/js'))
-);
+import config from '../../taskconfig';
+import gulp from 'gulp';
+import webpack from 'webpack';
+import $size from 'gulp-size';
+import $sourcemaps from 'gulp-sourcemaps';
+import $util from 'gulp-util';
+
+/**
+ * Compiles all JavaScript bundle files. This task assumes that all bundle files
+ * are located in /app/assets/js and ignores all sub-directories. File watching
+ * is available here.
+ *
+ * @param {boolean} debug
+ * @param {boolean} watch
+ */
+gulp.task('scripts', (callback) => {
+  let guard = false;
+
+  if (config.env.debug) {
+    return webpack(config.scripts).watch(100, build(callback));
+  }
+
+  webpack(config.scripts).run(build(callback));
+
+  function build(done) {
+    return (err, stats) => {
+      if (err) {
+        throw new $util.PluginError('webpack', err);
+      }
+
+      $util.log($util.colors.green('[webpack]'), stats.toString());
+
+      if (!guard && done) {
+        guard = true;
+        done();
+      }
+    };
+  }
+});
