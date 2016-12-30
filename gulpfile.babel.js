eb16f03b6fe59d279884c51bf09eb31b9d8cc85d
Rafaell Lycan <sonny.webdsg@gmail.com>
2016-12-30 19:13:31 +0100

    Add static files folders

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -20,7 +20,6 @@ gulp.task('default', () => {
   let seq = ['images', 'svgs', 'videos', 'fonts', 'styles', 'scripts'];
 
   if (config.env.debug) seq.push('watch');
-  if (!config.env.debug) seq.push('rev');
   sequence.apply(null, seq);
 });
 
